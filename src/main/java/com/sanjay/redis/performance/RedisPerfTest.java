073299bfa821508148e942bf7e43fd6f5c2de8b4
sanjaysharma <sanjay.gemini@gmail.com>
2021-01-20 16:06:13 +0000

    URL

--- src/main/java/com/sanjay/redis/performance/RedisPerfTest.java
+++ src/main/java/com/sanjay/redis/performance/RedisPerfTest.java
@@ -7,16 +7,18 @@ import org.redisson.config.Config;
 
 public class RedisPerfTest {
   public static void main(String[] args) {
-        String url = "rediss://10.154.0.2:6379";
+        String url = "redis://10.154.0.2:6379";
 
         Config config = new Config();
         config.useSingleServer()
                 // use "rediss://" for SSL connection
                 .setAddress(url);
         RedissonClient client = Redisson.create(config);
+        System.out.println("Starting work");
         long startTIime = System.currentTimeMillis();
 
         for (int i = 0; i < 1000; i++) {
+            System.out.println("Running batch " + i);
             for (int j = 0; j < 1000; j++) {
                 RMap<Object, Object> map = client.getMap("map"+i);
                 map.put("key"+j, 5.045d + i*j);
