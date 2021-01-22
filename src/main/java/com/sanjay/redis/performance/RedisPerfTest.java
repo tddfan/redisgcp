c17b0f7a3cc1f1da52e5bc253709d38c123202e8
sanjay <sanjay.gemini@gmail.com>
2021-01-22 12:36:55 +0000

    Performance test

--- src/main/java/com/sanjay/redis/performance/RedisPerfTest.java
+++ src/main/java/com/sanjay/redis/performance/RedisPerfTest.java
@@ -12,6 +12,7 @@ import java.util.List;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
+import org.redisson.api.RMapCache;
 
 public class RedisPerfTest {
   public static void main(String[] args) {
@@ -27,9 +28,9 @@ public class RedisPerfTest {
 
       ExecutorService service = Executors.newFixedThreadPool(8);
       List<Future<?>> futures = new ArrayList<>();
-      for (int i = 0; i < 100000; i++) {
+      for (int i = 0; i < 10000; i++) {
           final int batch = i;
-          Future<?> future = service.submit(() -> putMapBatch(client, batch));
+          Future<?> future = service.submit(() -> putBatch(client, batch));
           futures.add(future);
       }
       for (Future<?> future : futures) {
@@ -56,8 +57,8 @@ public class RedisPerfTest {
         long startTIime2 = System.currentTimeMillis();
         RBatch batch = client.createBatch();
         RMapAsync<Object, Object> map = batch.getMap("map" + i);
-        for (int j = 0; j < 1000; j++) {
-            map.fastPutAsync("key"+j +i, 5.045d + i*j);
+        for (int j = 0; j < 10000; j++) {
+            map.fastPutAsync("key"+j +i, new Value(i*j+"" , 5.045d + i*j, i*j+""));
         }
         batch.execute();
         System.out.println("Finishing batch " + i +  " in " + (System.currentTimeMillis() - startTIime2));
@@ -66,12 +67,36 @@ public class RedisPerfTest {
         System.out.println("Running batch " + i);
         long startTIime2 = System.currentTimeMillis();
         RMap<Object, Object> map = client.getMap("map" + i);
-        for (int j = 0; j < 1000; j++) {
+        for (int j = 0; j < 10000; j++) {
+            map.fastPut("key"+j +i, 5.045d + i*j);
+        }
+
+        System.out.println("Finishing batch " + i +  " in " + (System.currentTimeMillis() - startTIime2));
+    }
+    private static void putMapCache(RedissonClient client, int i) {
+        System.out.println("Running batch " + i);
+        long startTIime2 = System.currentTimeMillis();
+        RMapCache<Object, Object> map = client.getMapCache("map" + i);
+        for (int j = 0; j < 10000; j++) {
             map.fastPut("key"+j +i, 5.045d + i*j);
         }
 
         System.out.println("Finishing batch " + i +  " in " + (System.currentTimeMillis() - startTIime2));
     }
 
+
+}
+
+class Value {
+    String id;
+    double val;
+
+    public Value(String id, double val, String leg) {
+        this.id = id;
+        this.val = val;
+        this.leg = leg;
+    }
+
+    String leg;
 
 }
